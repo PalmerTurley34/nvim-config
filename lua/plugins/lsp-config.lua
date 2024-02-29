dfcbe78621eec0e393fe068aca997eb96ecaa1ef
PalmerTurley34 <palmerturley34@gmail.com>
2024-02-29 16:33:37 -0700

    neo-tree config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -49,9 +49,9 @@ return {
 			nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 			nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-			nmap("<leader>gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-			nmap("<leader>gr", vim.lsp.buf.references, "[G]oto [R]eferences")
-			nmap("<leader>gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+			nmap("<leader>gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+			nmap("<leader>gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+			nmap("<leader>gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
 			nmap("<leader>gh", vim.lsp.buf.hover, "Hover Documentation")
 			nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -30,6 +30,25 @@ return {
 		},
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>nt", ":Neotree filesystem reveal float<CR>", {})
+		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			event_handlers = {
+
+				{
+					event = "file_opened",
+					handler = function(file_path)
+						-- auto close
+						-- vimc.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+		vim.keymap.set("n", "<leader>nt", ":Neotree filesystem reveal left<CR>", {})
 	end,
 }
